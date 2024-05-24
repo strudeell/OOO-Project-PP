9a676d363b3f5a749a46c610c34134e6f1c6378c
strudeell <148209780+strudeell@users.noreply.github.com>
2024-05-24 23:04:20 +0500

    Update FindBorders.py
    Updating the code to find letter boundaries, loading the model, checking the segment for emptiness and final word recognition

--- FindBorders.py
+++ FindBorders.py
@@ -1,19 +1,18 @@
+import keras
+import tensorflow as tf
 import cv2
 import numpy as np
 from collections import defaultdict
+import matplotlib.pyplot as plt
 
-# работа с изображением
-
-original_img = cv2.imread("test(1).jpg")
+# Приведение изображения к нужному виду
+original_img = cv2.imread("test/1.jpg")
 img = original_img
-
-new_img = np.zeros(img.shape, dtype='uint8')
 img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 img = cv2.GaussianBlur(img, (5, 5), 0)
 img = cv2.Canny(img, 20, 20)
 
-# создаём список прямоугольных границ
-
+# Создаём список прямоугольных границ
 contours = cv2.findContours(img.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[0]
 boxes = []
 for cnt in contours:
@@ -23,8 +22,7 @@ for cnt in contours:
     if len(cv2.approxPolyDP(cnt, 10, True)) == 4:
         boxes.append(box)
 
-# считаем частоту встречаемости значений по y (верхние и нижноие границы)
-
+# Считаем частоту встречаемости значений по y (верхние и нижние границы)
 counts_y1 = defaultdict(int)
 counts_y2 = defaultdict(int)
 
@@ -34,8 +32,7 @@ for box in boxes:
     for point in box[2:]:
         counts_y2[point[1]] += 1
 
-# удаление дубликатов
-
+# Удаление дубликатов
 i = 0
 while i < len(boxes) - 1:
     coords_curr = [boxes[i][x][y] for x in range(4) for y in range(2)]
@@ -44,15 +41,14 @@ while i < len(boxes) - 1:
         boxes.pop(i)
     i += 1
 
-# поиск внешних контуров
-
+# Поиск внешних контуров
 counts_y1 = {k: v for k, v in sorted(counts_y1.items(), key=lambda item: item[1], reverse=True)}
 counts_y2 = {k: v for k, v in sorted(counts_y2.items(), key=lambda item: item[1], reverse=True)}
 lst_y1 = list(counts_y1)
 lst_y2 = list(counts_y2)
 y1, y2 = min(lst_y1[:2]), max(lst_y2[:2])  # y1- верхняя граница, y2 - нижняя
 
-# удаление областей, не являющихся границами
+# Удаление областей, не являющихся границами
 i = 0
 while i < len(boxes):
     box = boxes[i]
@@ -60,7 +56,8 @@ while i < len(boxes):
     if not (y1 in set(coords) and y2 in set(coords)):
         boxes.pop(i)
     i += 1
-# удаление внутренних границ
+
+# Удаление внутренних границ
 i = 0
 alpha = 1  # погрешность в пикселях
 
@@ -81,10 +78,47 @@ while i < len(boxes):
         boxes.pop(i)
     i += 1
 
-# отрисовка найденных областей
-
+# Отрисовка найденных областей
+img_with_contours = original_img.copy()
 for box in boxes:
-    cv2.drawContours(original_img, [box], 0, (255, 0, 0), 1)
-cv2.imshow('contours', original_img)  # вывод обработанного кадра в окно
+    cv2.drawContours(img_with_contours, [box], 0, (255, 0, 0), 1)
+plt.imshow(cv2.cvtColor(img_with_contours, cv2.COLOR_BGR2RGB))  # вывод обработанного кадра в окно
 cv2.waitKey()
 cv2.destroyAllWindows()
+
+# Загрузка модели нейросети
+class_names = ['!', '+', ',', '-', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '=', 'Ё', 'А', 'Б', 'В', 'Г',
+               'Д', 'Е', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч',
+               'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я']
+model_path = '23-6.35.h5'
+model = keras.models.load_model(model_path)
+
+result = ''
+alpha = 4
+for i in range(len(boxes)):
+    box = boxes[len(boxes) - i - 1]
+    # Получение координат выделенной области
+    x, y, w, h = cv2.boundingRect(box)
+    # Обрезка изображения по контуру
+    roi = original_img[y:y + h, x:x + w]
+    # Проверка на пустоту
+    roi_check = original_img[y + alpha:y + h - alpha, x + alpha:x + w - alpha]
+    roi_check = cv2.cvtColor(roi_check, cv2.COLOR_BGR2GRAY)
+    roi_check = cv2.GaussianBlur(roi_check, (5, 5), 0)
+    roi_check = cv2.Canny(roi_check, 20, 20)
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (7, 7))
+    closed = cv2.morphologyEx(roi_check, cv2.MORPH_CLOSE, kernel)
+    cnts = cv2.findContours(closed.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    if cnts == ((), None):
+        result += ' '
+        continue
+    # Приведение обрезанного изображения к нужному виду
+    roi_resized = cv2.resize(roi, (32, 32))
+    roi_array = tf.keras.utils.img_to_array(roi_resized)
+    roi_array = tf.expand_dims(roi_array, 0)
+    # Составление прогнозов нейросети
+    predictions = model.predict(roi_array)
+    score = tf.nn.softmax(predictions[0])
+    predicted_class_index = np.argmax(predictions)
+    result += class_names[np.argmax(score)]
+print(result)
